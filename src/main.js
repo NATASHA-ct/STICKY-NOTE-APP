ce9372541f7a0c761ddf999b263662cc30b74d1e
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-02-08 20:28:16 +0200

    Add basic empty functions

---
+++ src/main.js
@@ -0,0 +1,23 @@
+function loadNotes(){
+    console.log('Hello World');
+}
+
+function saveNotes(notes){
+
+}
+
+function createNoteCont(id, Content){
+
+}
+
+function addNote(notes) {
+
+}
+
+function editNote(id, newContent) {
+
+}
+
+function deleteNote(id) {
+    
+}
